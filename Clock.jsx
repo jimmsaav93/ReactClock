ccc032d628b20db2932cafc15176e822952ddc28
Jimmy Saavedra <119075977+jimmsaav93@users.noreply.github.com>
2024-05-27 07:51:41 -0500

    File clock

---
+++ Clock.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <h2>{time.toLocaleTimeString()}</h2>
+  );
+};
+
+export default Clock;
